257d35832128392b475553f164dda550325c6f3a
Ngoc Mai <hoangmai27022005@gmail.com>
2026-09-14 16:25:11 +0700

    Initial Commit

---
+++ GEO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GEO
+//
+//  Created by MAY 08 on 14/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GEO/GEOApp.swift
@@ -0,0 +1,17 @@
+//
+//  GEOApp.swift
+//  GEO
+//
+//  Created by MAY 08 on 14/9/26.
+//
+
+import SwiftUI
+
+@main
+struct GEOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
